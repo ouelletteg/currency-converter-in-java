92922f4000ae1ce09d530496c3cae85aa096ce29
Sou <amiti.amie@hotmail.com>
2023-12-05 20:42:51 -0500

    ajout de lib

---
+++ src/test/TestCurrency.java
@@ -0,0 +1,18 @@
+package test;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCurrency {
+
+    @Test
+    public void example(){
+
+        assertEquals(1, 1);
+
+    }
+
+    
+}
