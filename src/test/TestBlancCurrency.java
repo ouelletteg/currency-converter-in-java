972ea2c2203449761d5cc1460f345d6cd28884ff
ouelletteg <ouellettegenevieve@gmail.com>
2023-12-08 17:10:43 -0500

    test boite blanche currency

--- src/test/TestBlancCurrency.java
+++ src/test/TestBlancCurrency.java
@@ -5,23 +5,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestBlancCurrency {
-    // currencyConverter.Currency.convert(Double, Double)
-    //    public static Double convert(Double amount, Double exchangeValue) {
-    //        Double price;
-    //        price = amount * exchangeValue;
-    //        price = Math.round(price * 100d) / 100d;
-    //
-    //        return price;
-    //    }
+    //01 currencyConverter.Currency.convert(Double, Double)
+    //02    public static Double convert(Double amount, Double exchangeValue) {
+    //03        Double price;
+    //04        price = amount * exchangeValue;
+    //05        price = Math.round(price * 100d) / 100d;
+    //06
+    //07        return price;
+    //08    }
+
+    //A. D1 = {(x, y)|x est dans R (les réels) et y est dans R} 01-08
+    // Jeu de test : {(5,5)}
+    //B. Aucun if, pas de changement
+    //C. Aucune boucle, pas de changement
+    //D. Aucune boucle, pas de changement
+    //E. Aucune boucle, pas de changement
+
 
     @Test
-    public void example(){
+    public void testConvert(){
+        Double result = currencyConverter.Currency.convert(5.0, 5.0);
+        assertEquals(25, result);
 
-        assertEquals(1, 1);
-
+        Double result2 = currencyConverter.Currency.convert(10.0, 0.25);
+        assertEquals(2.5, result2);
     }
-
-
-
 
 }
